7510948ebee556541a8242e759d1b3a003867d8d
toystory23 <ghlduq@naver.com>
2023-08-09 00:36:34 +0900

    #6 Header component 구현

--- src/components/GNB/TotalGNB.tsx
+++ src/components/GNB/TotalGNB.tsx
@@ -1,11 +1,24 @@
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
 export const TotalGNB = () => {
-    return(
-    <div>
-        <span className="mr-16 ml-8 hover:cursor-pointer">이미지 자리</span>
-        <span className="mr-16 hover:cursor-pointer">정산 페이지</span>
-        <span className="mr-16 hover:cursor-pointer">결산 내역 조회</span>
-        <span className="mr-16 hover:cursor-pointer">냉장고 재고 현황</span>
-        <span className="mr-16 hover:cursor-pointer">요리별 DB</span>
-    </div>
-    )
-}
+  return (
+    <>
+      <Button variant="contained" >
+        <Link to="/">이미지자리</Link>
+      </Button>
+      <Button variant="contained" >
+        <Link to="/calculate">정산페이지</Link>
+      </Button>
+      <Button variant="contained" >
+        <Link to="/settlement">결산내역 조회</Link>
+      </Button>
+      <Button variant="contained" >
+        <Link to="/inventory">냉장고 재고 현황</Link>
+      </Button>
+      <Button variant="contained" >
+        <Link to="/cookingDB">요리별 DB</Link>
+      </Button>
+    </>
+  );
+};
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,13 @@
-import { TotalGNB } from "../GNB/TotalGNB"
-import { SignOut } from "../auth/SignOut"
+import { TotalGNB } from '../GNB/TotalGNB';
+import Button from '@mui/material/Button';
 
 export const Header = () => {
-    return(
-        <div className="flex justify-between mt-10">
-        <TotalGNB /> <SignOut/>
+  return (
+    <>
+      <div className='flex'>
+        <TotalGNB /> <div>OOO님 안녕하세요!</div><Button variant='contained'>LOG OUT</Button>
       </div>
-    )
-}
+      <Button variant="contained" style={{"background":"red"}}>test button</Button>
+    </>
+  );
+};
