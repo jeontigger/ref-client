d96064aa432109a09a4c784aabceb4241b70d267
jeontigger <boy9609@naver.com>
2023-08-09 03:08:49 +0900

    #9 DefaultValue를 의도에 맞게 Placeholder로 변경 및 비밀번호 일치 여부 확인 메시지 출력

--- src/components/auth/SignUpBox.tsx
+++ src/components/auth/SignUpBox.tsx
@@ -3,6 +3,7 @@ import {useRef} from 'react';
 export const SignUpBox = () => {
     // const [name, setFirstName] = useState('이름');
 
+    
     const ref = useRef<HTMLInputElement>(null);
     const handleClick = (e:any) => {
         e.preventDefault();
@@ -11,18 +12,36 @@ export const SignUpBox = () => {
         }
         console.log('handleclick');
     }
+    
+    const pwRef = useRef<HTMLInputElement>(null);
+    const pwCompareRef = useRef<HTMLInputElement>(null);
+    const CompareTextRef = useRef<HTMLSpanElement>(null);
+
+    const PWC = () => {
+        if(pwRef.current?.value === pwCompareRef.current?.value){
+            if (CompareTextRef.current) {
+                CompareTextRef.current.textContent = '비밀번호가 일치합니다!';
+              }
+        }else{
+            if (CompareTextRef.current) {
+                CompareTextRef.current.textContent = '비밀번호가 일치하지 않습니다!';
+              }
+        }
+    }
 
     return(
         <>
         
         <form >
-      <input type= "text" ref = {ref} defaultValue = {'name'}/><button type="submit">중복확인</button><br/>
-      <input type= "text" defaultValue = "비밀번호" /><br/>
-      <input type= "text" defaultValue = "비밀번호 확인" /><br/>
-      <input type= "text" defaultValue = "성명" /><br/>
-      <input type= "text" defaultValue = "생년월일" /><br/>
-      <input type= "text" defaultValue = "닉네임" /><br/>
-      <input type= "text" defaultValue = "이메일" /><br/>
+      <input type= "text" ref = {ref} placeholder = "이름" /><button type="submit">중복확인</button><br/>
+      <input type= "password" ref = {pwRef} placeholder = "password"  /><br/>
+      <input type= "password" ref = {pwCompareRef} placeholder = "password" onChange={PWC} /><br/>
+      <span ref = {CompareTextRef}>비밀번호를 입력해주세요</span><br/>
+      
+      <input type= "text" placeholder = "성명" /><br/>
+      <input type= "text" placeholder = "생년월일" /><br/>
+      <input type= "text" placeholder = "닉네임" /><br/>
+      <input type= "text" placeholder = "이메일" /><br/>
       <button type="submit">
         취소
       </button>
