78622cdb1be5c513cf7bb883b7fc6ff28d8d97a9
jeontigger <boy9609@naver.com>
2023-08-10 02:46:39 +0900

    #9 유효 검사 함수 구현 및 submit함수 구현

--- src/components/auth/SignUpBox.tsx
+++ src/components/auth/SignUpBox.tsx
@@ -13,12 +13,17 @@ export const SignUpBox = () => {
    */
   const idRef = useRef<HTMLInputElement>(null);
   /**
+   * ID 중복 검사 부울 변수
+   */
+  let idValidBool: boolean = false;
+  /**
    * ID 중복 체크하기위해 ID 인풋 박스의 Ref변수를 가져와 출력하는 함수.
    * 추후 ref변수로 ID를 중복데이터와 검사해야 함
    * @param e 리랜더링을 멈추기 위한 기법으로 잠시 사용
    */
   const IDDuplicateCheck = (e: any) => {
     console.log(idRef.current?.value);
+    idValidBool = true;
     e.preventDefault();
   };
 
@@ -35,6 +40,10 @@ export const SignUpBox = () => {
    */
   const CompareTextRef = useRef<HTMLSpanElement>(null);
   /**
+   * 비밀번호 유효 여부 부울 변수
+   */
+  let passwordValidBoolean: boolean = false;
+  /**
    * 비밀번호 일치 여부 출력 함수. password 박스와 passwordcheck 박스 모두 적용시켜야 함
    */
   const passwordConfirm = () => {
@@ -42,10 +51,12 @@ export const SignUpBox = () => {
       if (CompareTextRef.current) {
         CompareTextRef.current.textContent = "비밀번호가 일치합니다!";
       }
+      passwordValidBoolean = true;
     } else {
       if (CompareTextRef.current) {
         CompareTextRef.current.textContent = "비밀번호가 일치하지 않습니다!";
       }
+      passwordValidBoolean = false;
     }
   };
 
@@ -53,6 +64,10 @@ export const SignUpBox = () => {
    * 이메일 주소 체크를 위한 텍스트 박스 ref 변수
    */
   const emailValidTextRef = useRef<HTMLSpanElement>(null);
+  /**
+   * 이메일 유효 여부 부울 변수
+   */
+  let emailValidBoolean: boolean = false;
   /**
    * 이메일 주소 체크를 위한 함수
    */
@@ -65,21 +80,46 @@ export const SignUpBox = () => {
     if (emailRegex.test(email)) {
       if (emailValidTextRef.current) {
         emailValidTextRef.current.textContent = "이메일 형식이 일치합니다.";
+        emailValidBoolean = true;
       }
     } else {
       if (emailValidTextRef.current) {
         emailValidTextRef.current.textContent =
           "이메일 형식이 일치하지 않습니다.";
+        emailValidBoolean = false;
       }
     }
   };
-
+  const isAllValid = () => {
+    if (!idValidBool) return false;
+    if (!passwordValidBoolean) return false;
+    if (!emailValidBoolean) return false;
+    return true;
+  };
+  /**
+   * 데이터 제출을 위한 사용자 정의 타입
+   */
+  interface FormData {
+    [key: string]: any;
+  }
   /**
    * 미구현 상태의 제출 버튼 함수.
    * @param e 리랜더링을 멈추기 위한 기법으로 잠시 사용
    */
   const submitClick = (e: any) => {
+    const inputFields = document.querySelectorAll("input");
+    const formData: FormData = {};
+    if (!isAllValid()) {
+      console.log("not form");
+      return false;
+    }
+    for (let i = 0; i < inputFields.length; i++) {
+      const inputField = inputFields[i] as HTMLInputElement;
+      formData[inputField.name] = inputField.value;
+    }
+    console.log(formData);
     e.preventDefault();
+    return true;
   };
 
   /**
@@ -95,7 +135,13 @@ export const SignUpBox = () => {
     <>
       <form>
         <label>ID: </label>
-        <input type="text" ref={idRef} placeholder="아이디" />
+        <input
+          type="text"
+          ref={idRef}
+          placeholder="아이디"
+          name="id"
+          required
+        />
         <button type="submit" onClick={IDDuplicateCheck}>
           중복확인
         </button>
@@ -105,7 +151,9 @@ export const SignUpBox = () => {
           type="password"
           ref={pwRef}
           placeholder="비밀번호"
+          name="password"
           onChange={passwordConfirm}
+          required
         />
         <br />
         <label>비밀번호확인: </label>
@@ -113,21 +161,29 @@ export const SignUpBox = () => {
           type="password"
           ref={pwCompareRef}
           placeholder="비밀번호 확인"
+          name="passwordConfirm"
           onChange={passwordConfirm}
+          required
         />
         <br />
         <span ref={CompareTextRef}>비밀번호를 입력해주세요</span>
         <br />
         <label>성명: </label>
-        <input type="text" placeholder="성명" />
+        <input type="text" placeholder="성명" id="name" required />
         <br />
         <label>생년월일: </label>
         <DropDownCalendar />
         <br />
         <label>이메일: </label>
-        <input type="text" placeholder="이메일" onChange={validateEmail} />
+        <input
+          type="text"
+          placeholder="이메일"
+          name="email"
+          onChange={validateEmail}
+          required
+        />
         <br />
-        <span ref={emailValidTextRef}></span> <br />
+        <span ref={emailValidTextRef}>이메일을 입력하세요</span> <br />
         <button type="submit" onClick={cancelClick}>
           취소
         </button>
