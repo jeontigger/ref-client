543660e46071a42f9655a9c9742e629bb808368d
toystory23 <ghlduq@naver.com>
2023-08-14 23:21:34 +0900

    #15 Header CSS 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,14 @@ import Button from '@mui/material/Button';
 export const Header = () => {
   return (
     <>
-      <div className='flex'>
-        <TotalGNB /> <div>OOO님 안녕하세요!</div><Button variant='contained'>LOG OUT</Button>
+      <div className="flex justify-between mt-8">
+        <TotalGNB />{' '}
+        <div className='flex mr-4'>
+          <div className=' pr-4 content-center'>OOO님 안녕하세요!</div>
+          <Button variant="contained">LOG OUT</Button>
+        </div>
       </div>
-      <Button variant="contained" style={{"background":"red"}}>test button</Button>
+      {/* <Button variant="contained" style={{"background":"red"}}>test button</Button> */}
     </>
   );
 };
