407878d15a6ee75f747930809382159786a1c23d
toystory23 <ghlduq@naver.com>
2023-08-16 23:55:08 +0900

    ♻️#15 route 주소 동적변경

--- src/components/GNB/TotalGNB.tsx
+++ src/components/GNB/TotalGNB.tsx
@@ -1,24 +1,25 @@
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { RouteConst } from '../../interface/RouteConst';
 
 
 export const TotalGNB = () => {
   return (
     <div className=' px-2'>
       <Button variant="contained" sx={{mx:2}} >
-        <Link to="/">이미지자리</Link>
+        <Link to={RouteConst.HomePage}>이미지자리</Link>
       </Button>
       <Button variant="contained" sx={{mx:2}} >
-        <Link to="/calculate">정산페이지</Link>
+        <Link to={RouteConst.settlement}>정산페이지</Link>
       </Button>
       <Button variant="contained" sx={{mx:2}} >
-        <Link to="/settlement">결산내역 조회</Link>
+        <Link to={RouteConst.TotalReview}>결산내역 조회</Link>
       </Button>
       <Button variant="contained" sx={{mx:2}} >
-        <Link to="/inventory">냉장고 재고 현황</Link>
+        <Link to={RouteConst.Inventory}>냉장고 재고 현황</Link>
       </Button>
       <Button variant="contained" sx={{mx:2}} >
-        <Link to="/cookingDB">요리별 DB</Link>
+        <Link to={RouteConst.CookingDB}>요리별 DB</Link>
       </Button>
     </div>
   );
