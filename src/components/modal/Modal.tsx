13ea7246e5a4a16441bbcab0d3aab62c9c2f3cdc
jeontigger <boy9609@naver.com>
2023-08-26 04:02:41 +0900

    #25 모달 css 구현

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -40,14 +40,37 @@ export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   return (
     <>
       {display && (
-        <div className="modal">
-          <div className="modal-overlay" />
-          <div className="modal-content" ref={ref}>
-            <div className="modal-header">
-              <button onClick={handleClose}>Close</button>
-              <h2>Modal Header</h2>
+        <div
+          className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center "
+          style={{ backgroundColor: "rgba(128, 128, 128, 0.5)" }}
+        >
+          <div className="absolute bg-white shadow-lg rounded-lg" ref={ref}>
+            <div className="modal-header px-4 py-3">
+              <div className="flex justify-end items-center">
+                <button
+                  onClick={handleClose}
+                  className="text-gray-400 hover:text-black"
+                >
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex justify-center items-center">
+                <h2 className="text-lg font-medium">로그인</h2>
+              </div>
             </div>
-            <div className="modal-body">{children}</div>
+            <div className="modal-body p-4">{children}</div>
             <div className="modal-footer"></div>
           </div>
         </div>
